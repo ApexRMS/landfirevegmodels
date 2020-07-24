279c92d0943b4c4a836ab089927b42114955ffd2
alex <alexe_webmail@yahoo.com>
2020-07-24 16:26:36 -0700

    Added class library to package and updated .gitignore for binary builds.

---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LandFireVegModels")]
+[assembly: AssemblyDescription("LandFire Vegetation Models")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ApexRMS")]
+[assembly: AssemblyProduct("LandFireVegModels")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("Copyright © 2007-2020 Apex Resource Management Solutions Ltd. (ApexRMS). All rights reserved.")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("778fecb0-8b1a-4b1a-8c18-4e8038e0c23e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
