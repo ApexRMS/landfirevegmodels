87a27169242a029bde9038a7dd9feedb42216d97
Christopher Givelas <christopher.givelas@apexrms.com>
2025-12-04 10:07:08 -0500

    Remove obsolete files

--- src/Updates.cs
+++ src/Updates.cs
@@ -4,7 +4,7 @@ namespace LandFireVegModels
 {
     internal class Updates : DotNetUpdateProvider
     {
-        [UpdateAttribute(2.0, "This update serves as a new base schema version for landfirevegmodels in SyncroSim 3")]
+        [UpdateAttribute(2.0, "This update serves as a new base schema version for landfirevegmodels in SyncroSim 3.")]
         public static void Update_2_000(DataStore store)
         {
             // Fix typo (three 'c' in succession) in primary key column
