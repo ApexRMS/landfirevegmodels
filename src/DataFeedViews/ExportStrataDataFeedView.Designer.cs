d49e03251e4b6b05ec6b8c7afe40a7f8a4800001
Christopher Givelas <christopher.givelas@apexrms.com>
2025-12-04 14:48:21 -0500

    Add Anchors to ensure all view elements stay on-screen

--- src/DataFeedViews/ExportStrataDataFeedView.Designer.cs
+++ src/DataFeedViews/ExportStrataDataFeedView.Designer.cs
@@ -38,10 +38,13 @@
             // 
             // ExportStrataPanel
             // 
+            this.ExportStrataPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.ExportStrataPanel.AutoSize = true;
-            this.ExportStrataPanel.Location = new System.Drawing.Point(3, 69);
+            this.ExportStrataPanel.Location = new System.Drawing.Point(4, 69);
+            this.ExportStrataPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ExportStrataPanel.Name = "ExportStrataPanel";
-            this.ExportStrataPanel.Size = new System.Drawing.Size(919, 393);
+            this.ExportStrataPanel.Size = new System.Drawing.Size(1121, 500);
             this.ExportStrataPanel.TabIndex = 0;
             // 
             // ExportPathSelectionDialog
@@ -50,17 +53,22 @@
             // 
             // ExportPathTextBox
             // 
-            this.ExportPathTextBox.Location = new System.Drawing.Point(3, 29);
+            this.ExportPathTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ExportPathTextBox.Location = new System.Drawing.Point(4, 36);
+            this.ExportPathTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ExportPathTextBox.Name = "ExportPathTextBox";
             this.ExportPathTextBox.ReadOnly = true;
-            this.ExportPathTextBox.Size = new System.Drawing.Size(737, 20);
+            this.ExportPathTextBox.Size = new System.Drawing.Size(903, 22);
             this.ExportPathTextBox.TabIndex = 1;
             // 
             // BrowseButton
             // 
-            this.BrowseButton.Location = new System.Drawing.Point(757, 27);
+            this.BrowseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BrowseButton.Location = new System.Drawing.Point(915, 33);
+            this.BrowseButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.BrowseButton.Name = "BrowseButton";
-            this.BrowseButton.Size = new System.Drawing.Size(75, 23);
+            this.BrowseButton.Size = new System.Drawing.Size(100, 28);
             this.BrowseButton.TabIndex = 2;
             this.BrowseButton.Text = "Browse...";
             this.BrowseButton.UseVisualStyleBackColor = true;
@@ -68,9 +76,11 @@
             // 
             // ClearButton
             // 
-            this.ClearButton.Location = new System.Drawing.Point(847, 27);
+            this.ClearButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ClearButton.Location = new System.Drawing.Point(1023, 33);
+            this.ClearButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ClearButton.Name = "ClearButton";
-            this.ClearButton.Size = new System.Drawing.Size(75, 23);
+            this.ClearButton.Size = new System.Drawing.Size(100, 28);
             this.ClearButton.TabIndex = 3;
             this.ClearButton.Text = "Clear";
             this.ClearButton.UseVisualStyleBackColor = true;
@@ -79,15 +89,16 @@
             // ExportPathLabel
             // 
             this.ExportPathLabel.AutoSize = true;
-            this.ExportPathLabel.Location = new System.Drawing.Point(3, 9);
+            this.ExportPathLabel.Location = new System.Drawing.Point(4, 11);
+            this.ExportPathLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.ExportPathLabel.Name = "ExportPathLabel";
-            this.ExportPathLabel.Size = new System.Drawing.Size(90, 13);
+            this.ExportPathLabel.Size = new System.Drawing.Size(108, 16);
             this.ExportPathLabel.TabIndex = 4;
             this.ExportPathLabel.Text = "New Export Path:";
             // 
             // ExportStrataDataFeedView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.Controls.Add(this.ExportPathLabel);
@@ -95,8 +106,9 @@
             this.Controls.Add(this.BrowseButton);
             this.Controls.Add(this.ExportPathTextBox);
             this.Controls.Add(this.ExportStrataPanel);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "ExportStrataDataFeedView";
-            this.Size = new System.Drawing.Size(925, 465);
+            this.Size = new System.Drawing.Size(1129, 573);
             this.ResumeLayout(false);
             this.PerformLayout();
 
